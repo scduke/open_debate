f905cf4a746644712605a4a98ca8cf4a9265f9a6
steve <stephenchduke@gmail.com>
2014-11-03 19:13:05 -0600

    Added more detail to the Tournament Models.

--- Tournament/views.py
+++ Tournament/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+Views I want to have:
+
+    -home-page
+
+    -login page
+
+    -sign-up page
+
+    -create tournament page
+
+    -add things to tournaments page
+
+     -see all tournaments
+
+     -tournament detail page (should include a way to register for the tournament)
+
+     -student page
+"""
+
